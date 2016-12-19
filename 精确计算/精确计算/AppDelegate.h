b5bb6d812183230f826aa57143be3d9240501c9f
XLY <850401552@qq.com>
2016-12-19 18:30:14 +0800

    first

---
+++ 精确计算/精确计算/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  精确计算
+//
+//  Created by xuliying on 16/12/19.
+//  Copyright © 2016年 xly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 精确计算/精确计算/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  精确计算
+//
+//  Created by xuliying on 16/12/19.
+//  Copyright © 2016年 xly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
